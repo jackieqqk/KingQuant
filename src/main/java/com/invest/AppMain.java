2b10d167c1f939dc6500c091b899ee07ad6433ca
jackieqqk <dynamics.gao@gmail.com>
2019-11-29 20:48:17 +0900

    add main

---
+++ src/main/java/com/invest/AppMain.java
@@ -0,0 +1,11 @@
+package com.invest;
+
+/* 2019年11月29日20:44:20，by jackieqqk
+ * Anyone can use for free. Pls make it better.
+ */
+
+public class AppMain {
+    public static void main(String[] args){
+        System.out.println("KingQuant start");
+    }
+}
